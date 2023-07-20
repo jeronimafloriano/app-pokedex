0510e334a131ec8dbc5ed508c7124b029fe296dc
jeronima <jeronima.floriano@totvs.com.br>
2023-07-20 08:38:52 -0300

    Inserindo a busca dinâmica ao pesquisar pelos pokemons.

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'poke-search',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeSearchComponent implements OnInit {
 
+  @Output()
+  public emmitSearch: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public search(value: string) {
+    console.log(value);
+    this.emmitSearch.emit(value);
   }
 
 }
